b148e3299b118adb988fd28a873b38fa9055cfc5
Shramee Srivastav <shramee.srivastav@gmail.com>
2023-02-02 13:36:56 +0200

    #4 Add secret UI

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,2 +1,28 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1>Welcome to Shh-tarknet</h1>
+<article>
+	<header>
+		<h3 style="margin: 0">Create a secret</h3>
+	</header>
+	<form>
+		<label for="contract_addr">Contract address</label>
+		<input
+			type="text"
+			id="contract_addr"
+			name="contract_addr"
+			placeholder="Contract address"
+			required
+		/>
+
+		<label for="secret">Secret</label>
+		<input
+			type="text"
+			id="secret"
+			name="secret"
+			placeholder="Secret, comma separated for multiple values"
+			required
+		/>
+
+		<!-- Button -->
+		<button type="submit">Submit</button>
+	</form>
+</article>
