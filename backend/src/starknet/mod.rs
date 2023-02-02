d70c44975317020c95f56189581f1ea95af69e4a
Shramee Srivastav <shramee.srivastav@gmail.com>
2023-02-02 22:26:52 +0200

    #8 Starknet Wallet

--- backend/src/starknet/mod.rs
+++ backend/src/starknet/mod.rs
@@ -4,26 +4,54 @@ mod tests;
 use std::str::FromStr;
 
 use starknet::{
+    accounts::SingleOwnerAccount,
     core::{
+        chain_id,
         types::{BlockId, CallContractResult, CallFunction, FieldElement},
         utils::get_selector_from_name,
     },
     providers::{Provider, ProviderError, SequencerGatewayProvider},
+    signers::{LocalWallet, SigningKey},
 };
 
 pub struct StarknetManager {
     pub provider: SequencerGatewayProvider,
+    pub account: Option<SingleOwnerAccount<SequencerGatewayProvider, LocalWallet>>,
 }
 
 impl StarknetManager {
     pub async fn goerli() -> Self {
         Self {
             provider: SequencerGatewayProvider::starknet_alpha_goerli(),
+            account: None,
         }
     }
 
+    pub async fn setup(key_secret: &str, address: &str) -> Self {
+        Self {
+            provider: SequencerGatewayProvider::starknet_alpha_goerli(),
+            account: Some(Self::prepare_account(key_secret, address)),
+        }
+    }
+
+    pub fn prepare_account(
+        key_secret: &str,
+        address: &str,
+    ) -> SingleOwnerAccount<SequencerGatewayProvider, LocalWallet> {
+        let signer = LocalWallet::from(SigningKey::from_secret_scalar(
+            FieldElement::from_hex_be(key_secret).unwrap(),
+        ));
+        let address = FieldElement::from_hex_be(address).unwrap();
+        SingleOwnerAccount::new(
+            SequencerGatewayProvider::starknet_alpha_goerli(),
+            signer,
+            address,
+            chain_id::TESTNET,
+        )
+    }
+
     pub async fn call(
-        self,
+        &self,
         contract: &str,
         entrypoint: &str,
         calldata: Vec<&str>,
--- backend/src/starknet/tests.rs
+++ backend/src/starknet/tests.rs
@@ -1,30 +1,34 @@
-#[cfg(test)]
-mod tests {
-    use crate::starknet;
+use crate::starknet;
 
-    #[tokio::test]
-    async fn goerli_call() {
-        let manager = starknet::StarknetManager::goerli().await;
-        let resp = manager
-            .call(
-                "0x049d36570d4e46f48e99674bd3fcc84644ddd6b96f7c741b1562b82f9e004dc7",
-                "symbol",
-                vec![],
-            )
-            .await
-            .unwrap();
+#[tokio::test]
+async fn test_goerli_call() {
+    let manager = starknet::StarknetManager::goerli().await;
+    let resp = manager
+        .call(
+            "0x049d36570d4e46f48e99674bd3fcc84644ddd6b96f7c741b1562b82f9e004dc7",
+            "symbol",
+            vec![],
+        )
+        .await
+        .unwrap();
 
-        let res: Vec<String> = resp
-            .result
-            .iter()
-            .map(|f| {
-                let bytes = f.to_bytes_be().to_vec();
-                String::from_utf8(bytes.iter().filter(|v| **v > 0).map(|x| *x).collect()).unwrap()
-            })
-            .collect();
+    let res: Vec<String> = resp
+        .result
+        .iter()
+        .map(|f| {
+            let bytes = f.to_bytes_be().to_vec();
+            String::from_utf8(bytes.iter().filter(|v| **v > 0).map(|x| *x).collect()).unwrap()
+        })
+        .collect();
 
-        println!("Starknet call!");
-        println!("{res:#?}");
-        assert_eq!(&res[0], "ETH");
-    }
+    assert_eq!(&res[0], "ETH");
+}
+
+#[tokio::test]
+async fn test_add_account() {
+    let key_secret = "00ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff";
+    let address = "02da37a17affbd2df4ede7120dae305ec36dfe94ec96a8c3f49bbf59f4e9a9fa";
+    let manager = starknet::StarknetManager::setup(key_secret, address).await;
+
+    manager.account.expect("Died XO");
 }
