a0e7fd5ee27f5c697b431d563f07cc4cc47ba122
Shramee Srivastav <shramee.srivastav@gmail.com>
2023-02-03 03:20:25 +0200

    #14 Secrets manager

--- backend/src/secrets/mod.rs
+++ backend/src/secrets/mod.rs
@@ -1,22 +1,89 @@
 mod tests;
-use sha256::digest;
+use std::ops::Add;
 
+use sha256::digest;
+use sled::IVec;
+use starknet::core::types::FieldElement;
+
+#[derive(Debug)]
 pub struct Secret {
-    id: u128,
+    id: FieldElement,
     secret_hash: String,
     contract: String,
 }
 
+pub fn str_to_felt(data: &str) -> FieldElement {
+    let mut id_bytes: [u8; 32] = [0; 32];
+
+    for (i, byte) in data.as_bytes().iter().enumerate() {
+        id_bytes[31 - i] = *byte;
+    }
+    FieldElement::from_bytes_be(&id_bytes).unwrap()
+}
+
 impl Secret {
-    fn new(secret: &str, contract: &str) -> Secret {
+    fn new(id: &str, secret: &str, contract: &str) -> Self {
+        Self::new_from_hash(id, &digest(secret), contract)
+    }
+
+    fn new_from_hash(id: &str, secret_hash: &str, contract: &str) -> Self {
         Self {
-            id: 0,
-            secret_hash: digest(secret),
+            id: str_to_felt(&id),
+            secret_hash: secret_hash.into(),
             contract: contract.into(),
         }
     }
 
     fn compare(&self, candidate: &str) -> bool {
         self.secret_hash == digest(candidate)
+    }
+}
+
+pub struct Secrets_Manager {
+    db: sled::Db,
+}
+
+impl Secrets_Manager {
+    fn new() -> Self {
+        let mut path = std::env::current_dir().unwrap();
+        path.push(".data");
+        path.push("sled");
+        println!("The current directory is {}", path.display());
+        Self {
+            db: sled::open(path).unwrap(),
+        }
+    }
+
+    fn key(contract: &str, id: &FieldElement) -> String {
+        String::from(contract).add("::").add(&id.to_string())
+    }
+
+    fn save(&self, secret: Secret) -> sled::Result<Option<IVec>> {
+        self.db.insert(
+            &Self::key(&secret.contract, &secret.id),
+            secret.secret_hash.as_bytes(),
+        )
+    }
+
+    fn get(&self, contract: &str, id: &str) -> Option<Secret> {
+        let id_felt = str_to_felt(&id);
+        let hash_res = self.db.get(&Self::key(contract, &id_felt));
+        match hash_res {
+            sled::Result::Ok(v) => match v {
+                Some(hash) => {
+                    if hash.len() == 64 {
+                        Some(Secret::new_from_hash(
+                            id,
+                            &String::from_utf8(hash.to_vec()).unwrap(),
+                            contract,
+                        ))
+                    } else {
+                        None
+                    }
+                }
+                None => None,
+            },
+            sled::Result::Err(e) => None,
+        }
     }
 }
